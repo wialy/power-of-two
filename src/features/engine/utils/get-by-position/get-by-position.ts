34ee27e01a5b8d5cd40c373781243db120f734c2
Alex Ilchenko <ilczenko@gmail.com>
2024-03-06 16:30:35 +0100

    feat(engine): added getByPosition function

---
+++ src/features/engine/utils/get-by-position/get-by-position.ts
@@ -0,0 +1,22 @@
+import { Entity, Vector } from '../../types/entities';
+import { getIsSameVector } from '../get-is-same-vector';
+
+export const getByPosition = <T extends Entity>({
+	entities,
+	filter,
+	position,
+}: {
+	entities: Entity[];
+	position: Vector;
+	filter?: (entity: Entity) => entity is T;
+}): T | undefined => {
+	if (filter) {
+		return entities
+			.filter(filter)
+			.find((entity) => getIsSameVector(entity.position, position));
+	}
+
+	return entities.find((entity) =>
+		getIsSameVector(entity.position, position),
+	) as T;
+};
