868884db39df57d728f39194fd04f1c6de881690
Alex Ilchenko <ilczenko@gmail.com>
2024-03-06 16:32:04 +0100

    chore(engine): removed getHasMovingEntities function

--- src/features/engine/utils/get-has-moving-entities/get-has-moving-entities.ts
+++
@@ -1,7 +0,0 @@
-import { Entity, isMovable } from '../../types/entities';
-
-export const getHasMovingEntities = (entities: Entity[]): boolean =>
-	entities.some(
-		(entity) =>
-			isMovable(entity) && (entity.velocity.x !== 0 || entity.velocity.y !== 0),
-	);
