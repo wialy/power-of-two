7b56186c9ff2353361c779bc9f17c5bfd2567819
Alex Ilchenko <ilczenko@gmail.com>
2024-03-08 19:24:40 +0100

    feat(ui): extended useArrorSymbol function to use different sets

--- src/features/engine/utils/get-serialized/get-serialized.ts
+++ src/features/engine/utils/get-serialized/get-serialized.ts
@@ -1,12 +1,16 @@
-import { getArrowSymbol } from '../../../ui/utils/get-arrow-symbol';
+import {
+	ARROW_SET_TRIANGLE,
+	getArrowSymbol,
+} from '../../../ui/utils/get-arrow-symbol';
 import { VECTOR_ZERO } from '../../constants';
 import { Entity, isDice, isFloor, isMovable } from '../../types/entities';
+import { getBounds } from '../get-bounds';
 import { getIsSameVector } from '../get-is-same-vector';
 
 const getSymbol = (entity: Entity) => {
 	if (isFloor(entity)) {
 		return entity.direction
-			? getArrowSymbol(entity.direction)
+			? getArrowSymbol(entity.direction, ARROW_SET_TRIANGLE)
 			: entity.target
 				? '!'
 				: '□';
@@ -17,34 +21,6 @@ const getSymbol = (entity: Entity) => {
 	} else if (isMovable(entity)) {
 		return getArrowSymbol(entity.velocity);
 	}
-};
-
-const getBounds = ({ entities }: { entities: Entity[] }) => {
-	const topLeft = { x: Number.POSITIVE_INFINITY, y: Number.POSITIVE_INFINITY };
-	const bottomRight = {
-		x: Number.NEGATIVE_INFINITY,
-		y: Number.NEGATIVE_INFINITY,
-	};
-
-	for (const entity of entities) {
-		if (entity.position.x < topLeft.x) {
-			topLeft.x = entity.position.x;
-		}
-
-		if (entity.position.y < topLeft.y) {
-			topLeft.y = entity.position.y;
-		}
-
-		if (entity.position.x > bottomRight.x) {
-			bottomRight.x = entity.position.x;
-		}
-
-		if (entity.position.y > bottomRight.y) {
-			bottomRight.y = entity.position.y;
-		}
-	}
-
-	return { bottomRight, topLeft };
 };
 
 export const getSerialized = ({ entities }: { entities: Entity[] }) => {
--- src/features/ui/utils/get-arrow-symbol/get-arrow-symbol.ts
+++ src/features/ui/utils/get-arrow-symbol/get-arrow-symbol.ts
@@ -1,6 +1,14 @@
-export const getArrowSymbol = (velocity: { x: number; y: number }) => {
-	if (velocity.x === 0 && velocity.y === 0) return '⏹';
-	if (velocity.x === 0) return velocity.y > 0 ? '↓' : '↑';
+export const ARROW_SET_SIMPLE = '⏹↓↑→←';
+export const ARROW_SET_TRIANGLE = '⏹▲▼►◄';
 
-	return velocity.x > 0 ? '→' : '←';
+export const ARROW_SET_LETTERS = 'SUDRL';
+
+export const getArrowSymbol = (
+	velocity: { x: number; y: number },
+	set = ARROW_SET_SIMPLE,
+) => {
+	if (velocity.x === 0 && velocity.y === 0) return set[0];
+	if (velocity.x === 0) return velocity.y > 0 ? set[1] : set[2];
+
+	return velocity.x > 0 ? set[3] : set[4];
 };
