a0f863aa617abbd816b8d680226cc18bdcf6055e
Alex Ilchenko <ilczenko@gmail.com>
2024-03-18 19:35:51 +0100

    feat(editor): added getSymbolEntity function

---
+++ src/features/editor/utils/get-symbol-entity/get-symbol-entity.ts
@@ -0,0 +1,39 @@
+import { DIRECTIONS, VECTOR_ZERO } from '../../../engine/constants';
+import { createEntity } from '../../../engine/utils/create-entity';
+import { ID_SYMBOLS } from '../../constants';
+
+export const getSymbolEntity = (symbol: string) => {
+	if (symbol === ID_SYMBOLS.delimiter) {
+		return undefined;
+	}
+
+	if (symbol === ID_SYMBOLS.empty) {
+		return undefined;
+	}
+
+	if (ID_SYMBOLS.floor.includes(symbol)) {
+		return createEntity('floor');
+	}
+
+	if (ID_SYMBOLS.movable === symbol) {
+		return createEntity('movable');
+	}
+
+	if (ID_SYMBOLS.dice.includes(symbol)) {
+		return createEntity('dice', { value: Number(symbol) });
+	}
+
+	if (ID_SYMBOLS.directors.includes(symbol)) {
+		return createEntity('floor', {
+			direction: [VECTOR_ZERO, ...DIRECTIONS][
+				ID_SYMBOLS.directors.indexOf(symbol)
+			],
+		});
+	}
+
+	if (ID_SYMBOLS.target.includes(symbol)) {
+		return createEntity('floor', {
+			target: ID_SYMBOLS.target.indexOf(symbol),
+		});
+	}
+};
