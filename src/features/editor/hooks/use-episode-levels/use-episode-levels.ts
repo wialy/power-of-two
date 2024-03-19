51b644c91e91757a4ec4b506e8ca9eddc0c4dac9
Alex Ilchenko <ilczenko@gmail.com>
2024-03-19 15:07:50 +0100

    feat(editor): added useEpisodeLevels hooks

---
+++ src/features/editor/hooks/use-episode-levels/use-episode-levels.ts
@@ -0,0 +1,35 @@
+import { useEffect, useMemo, useState } from 'react';
+
+import { useGameState } from '../../../game/hooks/use-game-state';
+import { SerializedLevelRecord } from '../../types';
+import { useEpisodes } from '../use-episodes';
+
+export const useEpisodeLevels = () => {
+	const { episode } = useGameState();
+	const { episodes } = useEpisodes();
+	const [levels, setLevels] = useState<SerializedLevelRecord[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	const currentEpisode = useMemo(
+		() => episodes.find(({ symbols }) => symbols === episode),
+		[episodes, episode],
+	);
+
+	useEffect(() => {
+		const load = async () => {
+			if (!currentEpisode) {
+				return;
+			}
+
+			setIsLoading(true);
+			const response = await fetch(`/levels/${currentEpisode?.symbols}.json`);
+			const data = (await response.json()) as SerializedLevelRecord[];
+			setLevels(data);
+			setIsLoading(false);
+		};
+
+		void load();
+	}, [currentEpisode]);
+
+	return { isLoading, levels };
+};
