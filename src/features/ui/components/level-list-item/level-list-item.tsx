20c738efc5d7df04f07e214d6ad8157ee73a74f2
Alex Ilchenko <ilczenko@gmail.com>
2024-03-19 23:47:44 +0100

    feat(game): added locked levels

--- src/features/ui/components/level-list-item/level-list-item.tsx
+++ src/features/ui/components/level-list-item/level-list-item.tsx
@@ -1,19 +1,23 @@
-import { memo } from 'react';
+import { memo, useLayoutEffect, useRef } from 'react';
 
-import { MAX_GRID_HEIGHT, MAX_GRID_WIDTH } from '../../../editor/constants';
 import { LevelRecord } from '../../../editor/types';
 import { getIdEntities } from '../../../editor/utils/get-id-entities';
 import { getBounds } from '../../../engine/utils/get-bounds';
 import { MAX_MOVES_MULTIPLIER } from '../../../game/constants';
 import { useGameState } from '../../../game/hooks/use-game-state';
+import { Highscore } from '../../../game/types';
 import { TILE_SIZE } from '../../constants';
 import { EntityView } from '../entity-view';
 import $$ from './level-list-item.module.css';
 
 export const LevelListItem = ({
+	highscore,
+	isLocked,
 	level: { id, name, steps },
 }: {
 	level: LevelRecord;
+	highscore?: Highscore;
+	isLocked?: boolean;
 }) => {
 	const { setLevel, setMaxMoves, setScreen } = useGameState();
 
@@ -23,56 +27,88 @@ export const LevelListItem = ({
 		setScreen('game');
 	};
 
+	const reference = useRef<HTMLButtonElement>(null);
+
+	useLayoutEffect(() => {
+		if (
+			!isLocked &&
+			highscore === undefined &&
+			reference.current &&
+			reference.current.parentElement?.parentElement
+		) {
+			reference.current.parentElement.parentElement.scrollTo({
+				behavior: 'smooth',
+				top: reference.current.offsetTop - 100,
+			});
+		}
+	}, [isLocked, highscore]);
+
+	if (isLocked) {
+		return (
+			<button
+				disabled
+				className={$$.container}
+			>
+				<Preview
+					isLocked
+					id={id}
+				/>
+				<div className={$$.info}>
+					<div className={$$.name}>{name}</div>
+					<div className={$$.scores}>
+						<div>🔒 Locked</div>
+					</div>
+				</div>
+			</button>
+		);
+	}
+
 	return (
 		<button
+			ref={reference}
 			className={$$.container}
 			onClick={handleClick}
 		>
 			<Preview id={id} />
 			<div className={$$.info}>
 				<div className={$$.name}>{name}</div>
-				<div>🏆 {steps}</div>
+				<div className={$$.scores}>
+					{highscore !== undefined && <div>Best {highscore.moves}</div>}
+					<div>Pro {steps}</div>
+				</div>
 			</div>
 		</button>
 	);
 };
 
-const SCALE = 0.5;
+const SCALE = 0.2;
 
-const Preview = memo(({ id }: { id: string }) => {
+const Preview = memo(({ id, isLocked }: { id: string; isLocked?: boolean }) => {
 	const entities = getIdEntities(id);
 	const { bottomRight, topLeft } = getBounds({ entities });
 	const width = bottomRight.x - topLeft.x;
 	const height = bottomRight.y - topLeft.y;
 
 	return (
-		<div
-			style={{
-				alignItems: 'center',
-				backgroundColor: 'var(--color-navy)',
-				boxSizing: 'border-box',
-				display: 'flex',
-				height: TILE_SIZE * MAX_GRID_HEIGHT * SCALE,
-				justifyContent: 'center',
-				pointerEvents: 'none',
-				width: TILE_SIZE * (MAX_GRID_WIDTH + 3) * SCALE,
-			}}
-		>
-			<pre
-				style={{
-					height: height * TILE_SIZE * SCALE,
-					position: 'relative',
-					width: width * TILE_SIZE * SCALE,
-				}}
-			>
-				{entities.map((entity) => (
-					<EntityView
-						key={entity.id}
-						entity={entity}
-						scale={SCALE}
-					/>
-				))}
-			</pre>
+		<div className={$$.previewContainer}>
+			{Boolean(isLocked) && <div className={$$.locked}>🔒</div>}
+			{!isLocked && (
+				<pre
+					style={{
+						height: height * TILE_SIZE * SCALE,
+						position: 'relative',
+						width: width * TILE_SIZE * SCALE,
+					}}
+				>
+					{entities.map((entity) => (
+						<EntityView
+							key={entity.id}
+							entity={entity}
+							scale={SCALE}
+						/>
+					))}
+				</pre>
+			)}
 		</div>
 	);
 });
--- src/features/ui/components/levels-view/levels-view.tsx
+++ src/features/ui/components/levels-view/levels-view.tsx
@@ -1,10 +1,12 @@
 import { useEpisodeLevels } from '../../../editor/hooks/use-episode-levels';
 import { getParsedLevelRecord } from '../../../editor/utils/get-parsed-level-record';
+import { useHighscores } from '../../../game/hooks/use-highscores';
 import { LevelListItem } from '../level-list-item';
 import $$ from './levels-view.module.css';
 
 export const LevelsView = () => {
 	const { isLoading, levels } = useEpisodeLevels();
+	const { highscores } = useHighscores();
 
 	if (isLoading) {
 		return <div>Loading...</div>;
@@ -13,12 +15,32 @@ export const LevelsView = () => {
 	return (
 		<div className={$$.container}>
 			<div className={$$.levels}>
-				{levels.map((level) => (
-					<LevelListItem
-						key={level}
-						level={getParsedLevelRecord(level)}
-					/>
-				))}
+				{levels.map((level, index) => {
+					const { id } = getParsedLevelRecord(level);
+					const highscore = highscores.find(({ levelId }) => levelId === id);
+
+					let isLocked = false;
+					if (index > 0) {
+						const previousLevel = levels[index - 1];
+						const previousLevelId =
+							previousLevel && getParsedLevelRecord(previousLevel).id;
+
+						const previousHighscore = highscores.find(
+							({ levelId }) => levelId === previousLevelId,
+						);
+
+						isLocked = previousHighscore === undefined;
+					}
+
+					return (
+						<LevelListItem
+							key={level}
+							highscore={highscore}
+							isLocked={isLocked}
+							level={getParsedLevelRecord(level)}
+						/>
+					);
+				})}
 			</div>
 		</div>
 	);
