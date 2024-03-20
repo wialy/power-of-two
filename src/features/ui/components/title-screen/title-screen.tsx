cd1781e90c1503eb09004773cb1428a9749d87af
Alex Ilchenko <ilczenko@gmail.com>
2024-03-20 20:00:20 +0100

    feat(ui): added Discord and X links on title screen

--- src/features/ui/components/title-screen/title-screen.tsx
+++ src/features/ui/components/title-screen/title-screen.tsx
@@ -7,6 +7,14 @@ import { Logo } from '../logo';
 import { Screen } from '../screen';
 import $$ from './title-screen.module.css';
 
+const handleDiscordClick = () => {
+	window.open('https://discord.gg/DTBaDADspt', '_blank');
+};
+
+const handleTwitterClick = () => {
+	window.open('https://twitter.com/alexwooods', '_blank');
+};
+
 export const TitleScreen = () => {
 	const { screen, setScreen } = useGameState();
 	const isVisible = screen === 'title';
@@ -27,11 +35,28 @@ export const TitleScreen = () => {
 			<Logo />
 			<Button
 				size="large"
+				type="button"
 				onClick={handleStartClick}
 			>
 				<Icon name="pad" />
 				Play
 			</Button>
+			<div className={$$.social}>
+				<Button
+					type="button"
+					onClick={handleDiscordClick}
+				>
+					<Icon name="discord" />
+					Join Discord
+				</Button>
+				<Button
+					type="button"
+					onClick={handleTwitterClick}
+				>
+					<Icon name="x" />
+					Follow me
+				</Button>
+			</div>
 		</Screen>
 	);
 };
