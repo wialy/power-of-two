5351521c05ed0dd24e6ba8c677155ae559445655
Alex Ilchenko <ailchenko@hippo.com>
2024-03-26 19:15:57 +0100

    fix(engine): added check for removed entities in `getCanMerge` function

--- src/features/engine/utils/get-can-merge/get-can-merge.ts
+++ src/features/engine/utils/get-can-merge/get-can-merge.ts
@@ -1,4 +1,10 @@
 import { Entity, isDice } from '../../types/entities';
 
 export const getCanMerge = (a: Entity, b: Entity) =>
-	isDice(a) && isDice(b) && a.value === b.value && !a.isFresh && !b.isFresh;
+	isDice(a) &&
+	isDice(b) &&
+	a.value === b.value &&
+	!a.isFresh &&
+	!b.isFresh &&
+	!a.isRemoved &&
+	!b.isRemoved;
